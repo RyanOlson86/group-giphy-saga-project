8bbdd073e57d9e3f3b625aed081bc486daedbd04
Ryan Olson <ryan.olson86@yahoo.com>
2024-01-25 16:41:31 -0600

    added styling

--- src/components/GiphList/GiphList.jsx
+++ src/components/GiphList/GiphList.jsx
@@ -1,13 +1,16 @@
 import { useSelector } from 'react-redux'
+import './GiphList.css'
 
 const GiphList = () => {
     const reduxStore = useSelector(store => store.giphList)
 
     return (
-        <div>
+        <div className='all-gifs'>
             {reduxStore.map((gif, index) =>(
-                <img key={index} src={gif}/>
-                // <div>{gif}</div>
+                <div className='gif-box' key={index}>
+                    <img  src={gif}/>
+                    <button>Add to Favorites!</button>
+                </div>           
             ))}
         </div>
     )
